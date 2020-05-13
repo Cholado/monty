aa9d182e17947f1d3dba6756f4fd361a296684db
Cholado <sntgmtnz@gmail.com>
2020-05-13 14:43:16 +0000

    setting lexical analyzer file

---
+++ lexical_analyzer.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - Lexical analizer checks the monty script file from input
+ *
+ * @ac: arguments counter
+ * @av: arguments values
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE otherwise
+ */
+int main(int ac, char *av[])
+{
+}
